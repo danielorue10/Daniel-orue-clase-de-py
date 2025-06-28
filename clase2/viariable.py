1f54a9a51a169e1115d6408f9b065749b34c564c
Daniel Orue <danielorue@ejemplo.com>
2025-06-28 12:42:25 -0300

    finaliz

---
+++ clase2/viariable.py
@@ -0,0 +1,31 @@
+"""esto es un comentario de varias lineas
+tambien se pueden hacer comentarios de una sola linea"""
+# esto es un comentario de una sola linea
+
+
+
+print("Hola mundoo xd")
+
+entero = 10 
+flotante = 10.5
+cadena = "Hola, soy una cadena"
+booleano = True
+print (entero,  flotante, cadena, booleano  )
+print (type(entero), type(flotante), type(cadena), type(booleano))
+
+#operaciones aritmeticas
+print("sumando dos numeros  ")
+a = 10
+b = 5       
+print ("la suma de dos numeros es: ", a + b )
+print("restando dos numeros  ")
+print ("la resta de dos numeros es: ", a - b )  
+print("multiplicando dos numeros  ")
+print ("la multiplicacion de dos numeros es: ", a * b )
+print("dividiendo dos numeros  ")
+b=2
+print ("la division de dos numeros es: ", a / b )
+print("dividiendo dos numeros enteros  ")
+print ("la division de dos numeros enteros es: ", a // b )
+print("el resto de la division de dos numeros enteros  ")
+print ("el resto de la division de dos numeros enteros es: ", a % b )
