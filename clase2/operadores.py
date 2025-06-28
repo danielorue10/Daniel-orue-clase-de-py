579ac3fb4ac0972e92ea2e4c6205083820db758b
Daniel Orue <danielorue@ejemplo.com>
2025-06-28 12:53:39 -0300

    3.0

---
+++ clase2/operadores.py
@@ -0,0 +1,28 @@
+
+#a==b igual que
+#a!=b distinto de
+#a < b menor que
+#a > b mayor que
+#a <= b menor o igual que
+# a>= b mayor o igual que
+a = 100
+b = 100
+r=a ==b
+r=a != b
+
+print(r)
+r = a > b
+print(r)
+
+r = a != b
+print(r)
+r = a < b
+print(r)
+r = a > b
+print(r)
+r = a <= b
+print(r)
+r = a >= b
+print(r)
+# Operadores logicos
+# and, or, not
