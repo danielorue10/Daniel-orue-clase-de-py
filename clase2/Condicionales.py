50a7804dda856e5db4b8acfdf9c511b380c1d394
Daniel Orue <danielorue@ejemplo.com>
2025-06-28 13:35:27 -0300

    condicionales

---
+++ clase2/Condicionales.py
@@ -0,0 +1,43 @@
+
+
+
+dato =int (input("Introduce un numero: "))
+if dato > 0:
+    print("El numero es positivo")
+elif dato < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
+
+if dato % 2 == 0:
+    print("El numero es par")   
+else:
+    print("El numero es impar") 
+    
+    if dato % 3 == 0:
+        print("El numero es multiplo de 3")
+    else:
+        print("El numero no es multiplo de 3")
+
+if dato % 5 == 0:
+    print("El numero es multiplo de 5")
+
+else:
+    print("El numero no es multiplo de 5")
+if dato % 7 == 0:
+    print("El numero es multiplo de 7")
+elif dato % 11 == 0:
+    print("El numero es multiplo de 11")
+elif dato % 13 == 0:
+    print("El numero es multiplo de 13")
+    
+
+else:
+    print("El numero no es multiplo de 7")
+# Fin del ejercicio
+# Fin del ejercicio
+# Fin del ejercicio
+
+
+
+        
