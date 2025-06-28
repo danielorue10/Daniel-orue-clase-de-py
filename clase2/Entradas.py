8abbf2a9aeac13ddb88829752995009f25a3726d
Daniel Orue <danielorue@ejemplo.com>
2025-06-28 13:24:37 -0300

    entra

---
+++ clase2/Entradas.py
@@ -0,0 +1,7 @@
+#entrada de datos por teclado
+
+cadena = input("Introduce el nombre: ")
+print(f"El nombre es: {cadena}")
+       
+numero = int(input("Introduce la  version: "))
+print(f"La version es: {numero}")
