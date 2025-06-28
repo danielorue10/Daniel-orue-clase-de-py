494879138a2db8f4ed3b4ada7f1ce70695162a7c
Daniel Orue <danielorue@ejemplo.com>
2025-06-28 12:59:17 -0300

    operaores aritme

---
+++ clase2/aritmeticos.py
@@ -0,0 +1,18 @@
+#operaciones aritmeticas
+a=17
+b=18
+r = 0
+
+c = a+ b
+print("suma", c)
+d = a - b
+print("resta", d)
+e = a * b
+print("multiplicacion", e)
+f = a / b
+print("division", f)
+g = a // b
+print("division entera", g)
+h = a % b   
+print("modulo", h)
+
