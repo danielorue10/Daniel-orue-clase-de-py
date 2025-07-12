52a62f46e2148e4e0f716fc7fd434085b47bcbee
Daniel Orue <danielorue@ejemplo.com>
2025-07-12 12:35:40 -0300

    while-commit

---
+++ clase3/while.py
@@ -0,0 +1,8 @@
+#estructuras repetitivas
+#while
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1   # Incrementar el contador en 1
+print("Fin del bucle while")
+# Bucle while con una condición más compleja
