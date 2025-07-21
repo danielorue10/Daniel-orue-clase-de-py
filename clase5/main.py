5bbe59cc7dc62ce71e0175ed0bd7ed0bac3cb654
Daniel Orue <danielorue@ejemplo.com>
2025-07-21 20:43:52 -0300

    se realizan un cambio de puerto de 5000 a 5001

--- clase5/main.py
+++ clase5/main.py
@@ -9,4 +9,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
