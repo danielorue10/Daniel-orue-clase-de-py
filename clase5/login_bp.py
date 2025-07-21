e0e12e12f7ec15f6b81514167b88dee8b86f18f2
Daniel Orue <danielorue@ejemplo.com>
2025-07-21 20:36:02 -0300

    se realiza lo ultimo

---
+++ clase5/login_bp.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify
+from log import inicializacionVariables
+
+login_bp = Blueprint('login_bp', __name__, url_prefix='/login')
+
+@login_bp.route('/', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data:
+        return jsonify({"code": 400, "message": "JSON inválido"}), 400
+
+    user = data.get('user')
+    password = data.get('password')
+
+    if user == "daniel" and password == "123unida":
+        return jsonify({"code": 200, "message": "Login OK", "action": "grant_access"})
+    else:
+        return jsonify({"code": 401, "message": "Credenciales inválidas", "action": "deny_access"})
--- clase5/main.py
+++ clase5/main.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python3
 from flask import Flask
-from login import login 
-  
+from login_bp import login_bp  # Importa el blueprint del archivo login_bp.py
 
 app = Flask(__name__)
-
-##servicios rest
-
-app.register_blueprint(login)
+app.register_blueprint(login_bp)
 
 @app.route('/', methods=['GET'])
-def helloo():
+def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000, debug=True,use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
